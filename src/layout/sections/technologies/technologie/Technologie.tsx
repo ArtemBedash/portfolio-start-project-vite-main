dbe1c3678c8e459a78b7efc2f05a566aee122b39
ArtemBedash <bot300191@gmail.com>
2025-05-25 21:58:50 +0300

    add full project

---
+++ src/layout/sections/technologies/technologie/Technologie.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import {SkillBar} from "../../../../components/skillbar/SkillBar.tsx";
+
+type TechnologiePropsType = {
+    title: string;
+    width: string;
+
+
+}
+
+
+export const Technologie = (props: TechnologiePropsType) => {
+    return (
+        <StyledTechnologie>
+
+            <Title>{props.title}</Title>
+            <SkillBar width={props.width}/>
+        </StyledTechnologie>
+
+
+    );
+};
+
+
+const StyledTechnologie = styled.div`
+
+    background-color: #0F1624;
+`
+const Title = styled.h3`
+    margin: 0;
+    color: white;
+`
+
+
+// const SkillBar = styled.div`
+//   height: 18px;
+//   width: 100%;
+//   background-color: #162950;
+//   border-radius: 10px;
+//   margin-bottom: 10px;
+//   overflow: hidden;
+// `;
+//
+// const Fill = styled.div<{width:string}>`
+//     height: 100%;
+//     width:${props => props.width || "100%"};
+//     background: linear-gradient(
+//             to right,
+//             #13ADC7 0%,
+//             #6978D1 67%,
+//             #945DD6 100%
+//     );
+//
+// `;
+//
